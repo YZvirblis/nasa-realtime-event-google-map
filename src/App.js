a6d026d328a64bc1944e163dd884e1deb1f97b85
Yuriy Zvirblis <ogwhitiez@gmail.com>
2022-10-01 05:58:48 +0300

    fire markers done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import Map from "./components/Map.tsx";
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div>hello</div>;
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true);
+      const res = await fetch(
+        `https://eonet.gsfc.nasa.gov/api/v2.1/events?api_key${process.env.REACT_APP_NASA_API_KEY}`
+      );
+      const { events } = await res.json();
+
+      setEventData(events);
+      setLoading(false);
+      console.log(eventData);
+    };
+    fetchEvents();
+  }, []);
+
+  return (
+    <div>{!loading ? <Map eventData={eventData}></Map> : <h1>LOADING</h1>}</div>
+  );
 }
 
 export default App;
---
+++ src/components/LocationMarker.jsx
@@ -0,0 +1,14 @@
+import { Icon } from "@iconify/react";
+import locationIcon from "@iconify/icons-mdi/fire-alert";
+
+import React from "react";
+
+function LocationMarker({ lat, lng, onClick }) {
+  return (
+    <div className="location-marker" onClick={onClick}>
+      <Icon icon={locationIcon} className="location-icon" />
+    </div>
+  );
+}
+
+export default LocationMarker;
